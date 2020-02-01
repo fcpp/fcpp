569a867f072db1f4ad52cc87327eba17fcc1a19e
Giorgio Audrito <giorgio.audrito@gmail.com>
2020-01-31 20:41:53 -0600

    optimized nth_elements

--- lib/common/algorithm.hpp
+++ lib/common/algorithm.hpp
@@ -9,6 +9,7 @@
 #define FCPP_COMMON_ALGORITHM_H_
 
 #include <algorithm>
+#include <iterator>
 
 
 /**
@@ -17,25 +18,111 @@
 namespace fcpp {
 
 
-/**
- * @brief Sort a sequence just enough to fix a set of particular positions.
- * Assumes that the index list is sorted in increasing order, and all indexes are within range 0 .. (elast-efirst-1).
- *
- * @param efirst Random-access iterator to the first element.
- * @param elast  Random-access iterator after the last element.
- * @param ifirst Random-access iterator to the first index.
- * @param ilast  Random-access iterator after the last index.
- * @param offs   An offset to be applied to all indexes.
- */
-template <typename E, typename I>
-void nth_elements(E efirst, E elast, I ifirst, I ilast, size_t offs = 0) {
-    if (ifirst == ilast) return;
-    I imid = ifirst + (ilast-ifirst)/2;
-    E emid = efirst + (*imid-offs);
-    nth_element (efirst, emid,  elast);
-    nth_elements(efirst, emid,  ifirst, imid,  offs);
-    nth_elements(emid+1, elast, imid+1, ilast, *imid+1);
+//! @cond INTERNAL
+namespace details {
+    //! @brief Compares two elements with the standard comparator.
+    template <typename T>
+    bool std_compare (T x, T y) {
+        return x < y;
+    }
+
+    //! @brief The median between `a`, `b` and `c` is put into `first` (copied from gcc).
+    template <typename E, typename C>
+    void median_to_first(E first, E a, E b, E c, C comp) {
+        if (comp(*a, *b)) {
+            if (comp(*b, *c))
+                std::iter_swap(first, b);
+            else if (comp(*a, *c))
+                std::iter_swap(first, c);
+            else
+                std::iter_swap(first, a);
+        }
+        else if (comp(*a, *c))
+            std::iter_swap(first, a);
+        else if (comp(*b, *c))
+            std::iter_swap(first, c);
+        else
+            std::iter_swap(first, b);
+    }
+
+    //! @brief Partitions the interval between `first` and `last` around the `pivot` (copied from gcc).
+    template <typename E, typename C>
+    E pivot_partition(E first, E last, E pivot, C comp)
+    {
+        while (true) {
+            while (comp(*first, *pivot))
+                ++first;
+            --last;
+            while (comp(*pivot, *last))
+                --last;
+            if (!(first < last))
+                return first;
+            std::iter_swap(first, last);
+            ++first;
+        }
+    }
+
+    /**
+     * @brief Internal function for nth_elements. Adds two parameters:
+     *
+     * @param offs   An offset to be applied to all indexes.
+     * @param bound  A recursion bound before switching to bounded methods.
+     */
+    template <typename E, typename I, typename C>
+    void nth_elements(E efirst, E elast, I ifirst, I ilast, C comp, size_t offs, size_t bound) {
+        // no indexes
+        if (ifirst == ilast) return;
+        // one index
+        if (ilast-ifirst == 1) {
+            int n = *ifirst-offs;
+            // is the first: just a min will do
+            if (n == 0) {
+                E emin = std::min_element(efirst, elast, comp);
+                std::swap(*efirst, *emin);
+                return;
+            }
+            // is the last: just a max will do
+            if (n+1 == elast-efirst) {
+                E emax = std::max_element(efirst, elast, comp);
+                std::swap(*(elast-1), *emax);
+                return;
+            }
+            // just call nth_element from the library
+            nth_element(efirst, efirst+n, elast, comp);
+            return;
+        }
+        // many indices, few elements
+        if (8*(ilast-ifirst) > elast-efirst) {
+            std::sort(efirst, elast);
+            return;
+        }
+        // quick-select partitioning around
+        while (ilast-ifirst > 1 and 8*(ilast-ifirst) <= elast-efirst) {
+            if (bound == 0) {
+                // being slow, changing strategy
+                while (ilast-ifirst > 1 and 8*(ilast-ifirst) <= elast-efirst) {
+                    I imid = ifirst + (ilast-ifirst)/2;
+                    E emid = efirst + (*imid-offs);
+                    nth_element (efirst, emid,  elast,  comp);
+                    nth_elements(emid+1, elast, imid+1, ilast, comp, *imid+1, 0);
+                    elast = emid;
+                    ilast = imid;
+                }
+                break;
+            }
+            E emid = efirst + (elast-efirst)/2;
+            median_to_first(efirst, efirst+1, emid, elast-1, comp);
+            emid = pivot_partition(efirst+1, elast, efirst, comp);
+            I imid = std::lower_bound(ifirst, ilast, (emid-efirst)+offs);
+            --bound;
+            nth_elements(emid, elast, imid, ilast, comp, (emid-efirst)+offs, bound);
+            elast = emid;
+            ilast = imid;
+        }
+        nth_elements(efirst, elast, ifirst, ilast, comp, offs, 0);
+    }
 }
+//! @endcond INTERNAL
 
 
 /**
@@ -47,16 +134,27 @@ void nth_elements(E efirst, E elast, I ifirst, I ilast, size_t offs = 0) {
  * @param ifirst Random-access iterator to the first index.
  * @param ilast  Random-access iterator after the last index.
  * @param comp   A comparison functor.
- * @param offs   An offset to be applied to all indexes.
  */
 template <typename E, typename I, typename C>
-void nth_elements(E efirst, E elast, I ifirst, I ilast, C comp, size_t offs = 0) {
-    if (ifirst == ilast) return;
-    I imid = ifirst + (ilast-ifirst)/2;
-    E emid = efirst + (*imid-offs);
-    nth_element (efirst, emid,  elast,  comp);
-    nth_elements(efirst, emid,  ifirst, imid,  comp, offs);
-    nth_elements(emid+1, elast, imid+1, ilast, comp, *imid+1);
+inline void nth_elements(E efirst, E elast, I ifirst, I ilast, C comp) {
+    size_t log = 2;
+    while ((1<<log) < (elast-efirst)) ++log;
+    details::nth_elements(efirst, elast, ifirst, ilast, comp, 0, 2*log);
+}
+
+
+/**
+ * @brief Sort a sequence just enough to fix a set of particular positions.
+ * Assumes that the index list is sorted in increasing order, and all indexes are within range 0 .. (elast-efirst-1).
+ *
+ * @param efirst Random-access iterator to the first element.
+ * @param elast  Random-access iterator after the last element.
+ * @param ifirst Random-access iterator to the first index.
+ * @param ilast  Random-access iterator after the last index.
+ */
+template <typename E, typename I>
+inline void nth_elements(E efirst, E elast, I ifirst, I ilast) {
+    nth_elements(efirst, elast, ifirst, ilast, details::std_compare<typename std::iterator_traits<E>::value_type>);
 }
 
 
--- test/common/algorithm.cpp
+++ test/common/algorithm.cpp
@@ -13,10 +13,10 @@ TEST(AlgorithmTest, NthElements) {
     std::mt19937 rnd(42);
     std::vector<int> ev, iv, inum = {1, 3, 10, 30, 100, 300, 1000};
     
-    for (int i=0; i<1000; ++i) ev.push_back(i);
+    for (int i=0; i<10000; ++i) ev.push_back(i);
     for (int n : inum) {
         std::shuffle(ev.begin(), ev.end(), rnd);
-        std::uniform_int_distribution<int> distr(0, 1000-n);
+        std::uniform_int_distribution<int> distr(0, ev.size()-n);
         iv.clear();
         for (int i=0; i<n; ++i) iv.push_back(distr(rnd));
         std::sort(iv.begin(), iv.end());

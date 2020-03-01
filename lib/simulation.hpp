1a3ad6bb87df6d39b65e678a61d486904e7eee35
Giorgio Audrito <giorgio.audrito@gmail.com>
2020-02-29 22:11:30 -0600

    added simulation bundle header

---
+++ lib/simulation.hpp
@@ -0,0 +1,15 @@
+// Copyright © 2020 Giorgio Audrito. All Rights Reserved.
+
+/**
+ * @file simulation.hpp
+ * @brief Bundle including all the simulation component headers.
+ */
+
+#ifndef FCPP_SIMULATION_H_
+#define FCPP_SIMULATION_H_
+
+#include "lib/simulation/physical_connector.hpp"
+#include "lib/simulation/physical_position.hpp"
+#include "lib/simulation/spawner.hpp"
+
+#endif // FCPP_SIMULATION_H_

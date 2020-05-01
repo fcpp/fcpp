95e98af06c3e702e87576711344297e826ff9261
Giorgio Audrito <giorgio.audrito@gmail.com>
2020-05-01 08:54:28 +0200

    added tuple object empowered with pointwise operations

---
+++ src/lib/data/tuple.hpp
@@ -0,0 +1,424 @@
+// Copyright © 2020 Giorgio Audrito. All Rights Reserved.
+
+/**
+ * @file tuple.hpp
+ * @brief Implementation of the `tuple` class extending `std::tuple` with point-wise operators.
+ */
+
+#ifndef FCPP_DATA_TUPLE_H_
+#define FCPP_DATA_TUPLE_H_
+
+#include <tuple>
+#include <type_traits>
+
+
+/**
+ * @brief Namespace containing all the objects in the FCPP library.
+ */
+namespace fcpp {
+
+
+/**
+ * @brief Object holding a collection of elements of possibly different types, as `std::tuple` with point-wise operators.
+ */
+//! @{
+//! @brief General case.
+template <typename... Ts>
+class tuple {
+    //! @cond INTERNAL
+    template <typename... Us> friend class tuple;
+    //! @endcond
+    
+  public:
+    //! @brief constructors
+    //! @{
+    //! @brief Default constructor.
+    constexpr tuple() = default;
+    //! @brief Copy constructor.
+    tuple(const tuple&) = default;
+    //! @brief Move constructor.
+    tuple(tuple&&) = default;
+    //! @brief Implicit conversion copy constructor.
+    template <typename... Us, typename = std::enable_if_t<sizeof...(Us) == sizeof...(Ts)>>
+    tuple(const tuple<Us...>& t) : m_tuple(t.m_tuple) {}
+    //! @brief Implicit conversion move constructor.
+    template <typename... Us, typename = std::enable_if_t<sizeof...(Us) == sizeof...(Ts)>>
+    tuple(tuple<Us...>&& t) : m_tuple(std::move(t.m_tuple)) {}
+    //! @brief Initialization copy constructor.
+    tuple(const Ts&... xs) : m_tuple(xs...) {}
+    //! @brief Initialization move and convert constructor.
+    template <typename... Us, typename = std::enable_if_t<sizeof...(Us) >= 2>>
+    tuple(Us&&... xs) : m_tuple(std::move(xs)...) {}
+    //! @}
+    
+    //! @brief assignment
+    //! @{
+    //! @brief Copy assignment.
+    tuple& operator=(const tuple& t) = default;
+    //! @brief Move assignment.
+    tuple& operator=(tuple&& t) = default;
+    //! @brief Implicit conversion copy assignment.
+    template <typename... Us>
+    tuple& operator=(const tuple<Us...>& t) {
+        m_tuple = t.m_tuple;
+        return *this;
+    }
+    //! @brief Implicit conversion move assignment.
+    template <typename... Us>
+    tuple& operator=(tuple<Us...>&& t) {
+        m_tuple = std::move(t.m_tuple);
+        return *this;
+    }
+    //! @}
+    
+    //! @brief Exchanges the content of the `tuple` objects.
+    void swap(tuple& t) {
+        m_tuple.swap(t.m_tuple);
+    }
+    
+  private:
+    std::tuple<Ts...> m_tuple;
+};
+
+//! @brief Special case of empty tuple.
+template <>
+class tuple<> {
+    //! @cond INTERNAL
+    template <typename... Us> friend class tuple;
+    //! @endcond
+    
+  public:
+    //! @brief constructors
+    //! @{
+    //! @brief Default constructor.
+    constexpr tuple() = default;
+    //! @brief Copy constructor.
+    tuple(const tuple&) = default;
+    //! @brief Move constructor.
+    tuple(tuple&&) = default;
+    //! @}
+    
+    //! @brief assignment
+    //! @{
+    //! @brief Copy assignment.
+    tuple& operator=(const tuple& t) = default;
+    //! @brief Move assignment.
+    tuple& operator=(tuple&& t) = default;
+    //! @}
+    
+    //! @brief Exchanges the content of the `tuple` objects.
+    void swap(tuple&) {}
+};
+//! @}
+
+
+//! @cond INTERNAL
+namespace details {
+    //! @brief Helper class unwrapping `std::reference_wrapper` objects (general case).
+    template <typename T>
+    struct unwrap_refwrapper {
+        using type = T;
+    };
+     //! @brief Helper class unwrapping `std::reference_wrapper` objects (special case).
+    template <typename T>
+    struct unwrap_refwrapper<std::reference_wrapper<T>> {
+        using type = T&;
+    };
+
+    //! @brief Decays a type, eliminating reference wrappers.
+    template <typename T>
+    using special_decay_t = typename unwrap_refwrapper<std::decay_t<T>>::type;
+
+    //! @brief Converts an `std::tuple` into a `tuple` (rvalue).
+    template <typename... Ts>
+    tuple<Ts...> tuple_promote(std::tuple<Ts...>&& t) {
+        return (tuple<Ts...>&&)t;
+    }
+    //! @brief Converts an `std::tuple` into a `tuple` (lvalue).
+    template <typename... Ts>
+    const tuple<Ts...>& tuple_promote(const std::tuple<Ts...>& t) {
+        return (const tuple<Ts...>&)t;
+    }
+
+    //! @brief Converts a `tuple` into an `std::tuple` (rvalue).
+    template <typename... Ts>
+    std::tuple<Ts...> tuple_demote(tuple<Ts...>&& t) {
+        return (std::tuple<Ts...>&&)t;
+    }
+    //! @brief Converts a `tuple` into an `std::tuple` (lvalue).
+    template <typename... Ts>
+    const std::tuple<Ts...>& tuple_demote(const tuple<Ts...>& t) {
+        return (const std::tuple<Ts...>&)t;
+    }
+}
+//! @endcond
+
+
+//! @brief Constructs an appropriate `tuple` containing the elements specified in xs, as `std::make_tuple`.
+template <typename... Ts>
+tuple<details::special_decay_t<Ts>...> make_tuple(Ts&&... xs) {
+    return {std::forward<Ts>(xs)...};
+}
+
+
+//! @brief Construct a `tuple` suitable to be forwarded as argument to a function, as `std::forward_as_tuple`.
+template <typename... Ts>
+constexpr tuple<Ts&&...> forward_as_tuple(Ts&&... xs) noexcept {
+    return {std::forward<Ts>(xs)...};
+}
+
+
+//! @brief Concatenate the tuples given as arguments, as `std::tuple_cat`.
+template <typename... Ts>
+auto tuple_cat(Ts&&... ts) {
+    return details::tuple_promote(std::tuple_cat(details::tuple_demote(std::forward<Ts>(ts))...));
+}
+
+
+/**
+ * @name get element
+ *
+ * Accesses an element of a tuple, as `std::get`.
+ */
+//! @{
+//! @brief lvalue overload
+template <size_t n, typename... Ts>
+typename std::tuple_element<n, std::tuple<Ts...>>::type& get(tuple<Ts...>& t) noexcept {
+    return std::get<n>((std::tuple<Ts...>&)t);
+}
+//! @brief rvalue overload
+template <size_t n, typename... Ts>
+typename std::tuple_element<n, std::tuple<Ts...>>::type&& get(tuple<Ts...>&& t) noexcept {
+    return std::get<n>((std::tuple<Ts...>&&)t);
+}
+//! @brief const lvalue overload
+template <size_t n, typename... Ts>
+typename std::tuple_element<n, std::tuple<Ts...>>::type const& get(const tuple<Ts...>& t) noexcept {
+    return std::get<n>((const std::tuple<Ts...>&)t);
+}
+//! @}
+
+
+//! @brief Object ignoring any value assigned to it, as `std::ignore`.
+using std::ignore;
+
+
+/**
+ * @brief Tie arguments to tuple elements, as `std::tie`.
+ *
+ * Constructs a `tuple` object whose elements are references to the arguments, in the same order.
+ * This allows a set of objects to act as a tuple, which is especially useful to unpack `tuple` objects.
+ * The special constant `ignore` can be used to specify elements of a tuple to be ignored instead of
+ * tied to a specific object.
+ */
+template <typename... Ts>
+constexpr tuple<Ts&...> tie(Ts&... args) noexcept {
+    return {args...};
+}
+
+
+//! @brief Swaps contents of x and y, as `std::swap`.
+template <typename... Ts>
+void swap(tuple<Ts...>& x, tuple<Ts...>& y) noexcept {
+    x.swap(y);
+}
+
+
+//! @cond INTERNAL
+namespace details {
+    //! @brief A reference to a tuple, together with an index sequence as parameter.
+    template <typename T, typename I>
+    struct tuple_wrapper {
+        //! @brief Constructor from a reference.
+        tuple_wrapper(T t) : m_tuple(static_cast<T>(t)) {}
+        
+        //! @brief Accesses the tuple.
+        T tuple() {
+            return static_cast<T>(m_tuple);
+        }
+        
+      private:
+        //! @brief The internal reference.
+        T m_tuple;
+    };
+    
+    //! @brief Wraps a tuple, depending on its lvalue/rvalue/const lvalue status.
+    //! @{
+    //! @brief lvalue overload
+    template <typename... Ts>
+    inline tuple_wrapper<tuple<Ts...>&, std::make_index_sequence<sizeof...(Ts)>> tuple_wrap(tuple<Ts...>& x) {
+        return {x};
+    }
+    //! @brief rvalue overload
+    template <typename... Ts>
+    inline tuple_wrapper<tuple<Ts...>&&, std::make_index_sequence<sizeof...(Ts)>> tuple_wrap(tuple<Ts...>&& x) {
+        return {std::move(x)};
+    }
+    //! @brief const lvalue overload
+    template <typename... Ts>
+    inline tuple_wrapper<const tuple<Ts...>&, std::make_index_sequence<sizeof...(Ts)>> tuple_wrap(const tuple<Ts...>& x) {
+        return {x};
+    }
+    //! @}
+
+    //! @brief Empty function ignoring its arguments, for allowing pack expansion.
+    template <typename... Ts>
+    void ignore_args(Ts...) {}
+
+    //! @brief Macros defining every operator on a tuple_wrapper pointwise on the referenced tuple.
+    //! @{
+    #define _DEF_UOP(op)                                                    \
+    template <typename T, size_t... is>                                     \
+    auto operator op(tuple_wrapper<T, std::index_sequence<is...>> x) {      \
+        return fcpp::make_tuple((op get<is>(x.tuple()))...);                      \
+    }
+
+    #define _DEF_BOP(op)                                                    \
+    template <typename T, typename U, size_t... is>                         \
+    auto operator op(tuple_wrapper<T, std::index_sequence<is...>> x,        \
+                    tuple_wrapper<U, std::index_sequence<is...>> y) {       \
+        return fcpp::make_tuple((get<is>(x.tuple()) op get<is>(y.tuple()))...);   \
+    }
+
+    #define _DEF_IOP(op)                                                    \
+    template <typename T, typename U, size_t... is>                         \
+    auto operator op##=(tuple_wrapper<T, std::index_sequence<is...>> x,     \
+                     tuple_wrapper<U, std::index_sequence<is...>> y) {      \
+        ignore_args((get<is>(x.tuple()) op##= get<is>(y.tuple()))...);      \
+        return x.tuple();                                                   \
+    }
+
+    _DEF_UOP(+)
+    _DEF_UOP(-)
+    _DEF_UOP(~)
+    _DEF_UOP(!)
+
+    _DEF_BOP(+)
+    _DEF_BOP(-)
+    _DEF_BOP(*)
+    _DEF_BOP(/)
+    _DEF_BOP(%)
+    _DEF_BOP(^)
+    _DEF_BOP(&)
+    _DEF_BOP(|)
+    _DEF_BOP(<)
+    _DEF_BOP(>)
+    _DEF_BOP(<=)
+    _DEF_BOP(>=)
+    _DEF_BOP(==)
+    _DEF_BOP(!=)
+    _DEF_BOP(&&)
+    _DEF_BOP(||)
+    _DEF_BOP(<<)
+    _DEF_BOP(>>)
+
+    _DEF_IOP(+)
+    _DEF_IOP(-)
+    _DEF_IOP(*)
+    _DEF_IOP(/)
+    _DEF_IOP(%)
+    _DEF_IOP(^)
+    _DEF_IOP(&)
+    _DEF_IOP(|)
+    _DEF_IOP(>>)
+    _DEF_IOP(<<)
+
+    #undef _DEF_UOP
+    #undef _DEF_BOP
+    #undef _DEF_IOP
+    //! @}
+}
+//! @endcond
+
+
+//! @cond INTERNAL
+//! @brief Macros defining every operator pointwise on a tuple.
+#define _DEF_UOP(op)                                                                \
+template <typename... Ts>                                                           \
+auto operator op(tuple<Ts...>&& x) {                                                \
+    return op details::tuple_wrap(std::move(x));                                    \
+}                                                                                   \
+template <typename... Ts>                                                           \
+auto operator op(const tuple<Ts...>& x) {                                           \
+    return op details::tuple_wrap(x);                                               \
+}
+
+#define _DEF_BOP(op)                                                                \
+template <typename... Ts, typename... Us,                                           \
+          typename = std::enable_if_t<sizeof...(Ts) == sizeof...(Us)>>              \
+auto operator op(const tuple<Ts...>& x, const tuple<Us...>& y) {                    \
+    return details::tuple_wrap(x) op details::tuple_wrap(y);                        \
+}                                                                                   \
+template <typename... Ts, typename... Us,                                           \
+          typename = std::enable_if_t<sizeof...(Ts) == sizeof...(Us)>>              \
+auto operator op(tuple<Ts...>&& x, const tuple<Us...>& y) {                         \
+    return details::tuple_wrap(std::move(x)) op details::tuple_wrap(y);             \
+}                                                                                   \
+template <typename... Ts, typename... Us,                                           \
+          typename = std::enable_if_t<sizeof...(Ts) == sizeof...(Us)>>              \
+auto operator op(const tuple<Ts...>& x, tuple<Us...>&& y) {                         \
+    return details::tuple_wrap(x) op details::tuple_wrap(std::move(y));             \
+}                                                                                   \
+template <typename... Ts, typename... Us,                                           \
+          typename = std::enable_if_t<sizeof...(Ts) == sizeof...(Us)>>              \
+auto operator op(tuple<Ts...>&& x, tuple<Us...>&& y) {                              \
+    return details::tuple_wrap(std::move(x)) op details::tuple_wrap(std::move(y));  \
+}
+
+#define _DEF_IOP(op)                                                                \
+template <typename... Ts, typename... Us,                                           \
+          typename = std::enable_if_t<sizeof...(Ts) == sizeof...(Us)>>              \
+auto operator op##=(tuple<Ts...>& x, const tuple<Us...>& y) {                       \
+    return details::tuple_wrap(x) op##= details::tuple_wrap(y);                     \
+}                                                                                   \
+template <typename... Ts, typename... Us,                                           \
+          typename = std::enable_if_t<sizeof...(Ts) == sizeof...(Us)>>              \
+auto operator op##=(tuple<Ts...>& x, tuple<Us...>&& y) {                            \
+    return details::tuple_wrap(x) op##= details::tuple_wrap(std::move(y));          \
+}
+
+
+_DEF_UOP(+)
+_DEF_UOP(-)
+_DEF_UOP(~)
+_DEF_UOP(!)
+
+_DEF_BOP(+)
+_DEF_BOP(-)
+_DEF_BOP(*)
+_DEF_BOP(/)
+_DEF_BOP(%)
+_DEF_BOP(^)
+_DEF_BOP(&)
+_DEF_BOP(|)
+_DEF_BOP(<)
+_DEF_BOP(>)
+_DEF_BOP(<=)
+_DEF_BOP(>=)
+_DEF_BOP(==)
+_DEF_BOP(!=)
+_DEF_BOP(&&)
+_DEF_BOP(||)
+_DEF_BOP(<<)
+_DEF_BOP(>>)
+
+_DEF_IOP(+)
+_DEF_IOP(-)
+_DEF_IOP(*)
+_DEF_IOP(/)
+_DEF_IOP(%)
+_DEF_IOP(^)
+_DEF_IOP(&)
+_DEF_IOP(|)
+_DEF_IOP(>>)
+_DEF_IOP(<<)
+
+#undef _DEF_UOP
+#undef _DEF_BOP
+#undef _DEF_IOP
+//! @endcond
+
+
+}
+
+#endif // FCPP_DATA_TUPLE_H_
---
+++ src/test/data/tuple.cpp
@@ -0,0 +1,98 @@
+// Copyright © 2020 Giorgio Audrito. All Rights Reserved.
+
+#include "gtest/gtest.h"
+
+#include "lib/data/tuple.hpp"
+
+using namespace fcpp;
+
+
+TEST(TupleTest, Constructors) {
+    tuple<int,bool> t{2, false};
+    tuple<int,bool> u{t};
+    tuple<double,int> s{t};
+    s = u;
+    u = std::move(t);
+    t = u;
+    swap(t, u);
+    EXPECT_EQ(get<0>(t), get<0>(u));
+    EXPECT_EQ(get<1>(t), get<1>(u));
+}
+
+TEST(TupleTest, Makers) {
+    tuple<int,bool> t = make_tuple(42, true);
+    tuple<int,bool> u = forward_as_tuple(get<0>(t), get<1>(t));
+    int  i;
+    bool b;
+    tie(i, b) = u;
+    EXPECT_EQ(42, i);
+    EXPECT_EQ(true, b);
+}
+
+TEST(TupleTest, TupleCat) {
+    tuple<int,double> t{10,4.5};
+    tuple<bool,char> u{false,'x'};
+    tuple<> w = make_tuple();
+    tuple<int,double,bool,char> s = tuple_cat(t, u, w);
+    EXPECT_EQ(10,    get<0>(s));
+    EXPECT_EQ(4.5,   get<1>(s));
+    EXPECT_EQ(false, get<2>(s));
+    EXPECT_EQ('x',   get<3>(s));
+}
+
+TEST(TupleTest, Operators) {
+    tuple<int,double> t{5,6};
+    tuple<int,double> s = -t;
+    s = -tuple<int,double>{t};
+    EXPECT_EQ(-5,   get<0>(s));
+    EXPECT_EQ(-6.0, get<1>(s));
+    s += t;
+    s += tuple<int,double>{t};
+    EXPECT_EQ(5,   get<0>(s));
+    EXPECT_EQ(6.0, get<1>(s));
+    tuple<int,double> u;
+    u = s + t;
+    u = s + tuple<int,double>{t};
+    u = tuple<int,double>{s} + t;
+    u = tuple<int,double>{s} + tuple<int,double>{t};
+    EXPECT_EQ(10,   get<0>(u));
+    EXPECT_EQ(12.0, get<1>(u));
+    auto x = (s <= u);
+    EXPECT_EQ(true, get<0>(x));
+    EXPECT_EQ(true, get<1>(x));
+}
+
+TEST(TupleTest, NestedTuples) {
+    tuple<tuple<int,char>,bool> s{{2,1},false}, t{{3,4},true}, u, x;
+    u = {{1,5},true};
+    x = make_tuple(tuple<int,char>{2,'a'}, true);
+    x = s & t;
+    EXPECT_EQ(2,        get<0>(get<0>(x)));
+    EXPECT_EQ(char(0),  get<1>(get<0>(x)));
+    EXPECT_EQ(false,    get<1>(x));
+    x |= u;
+    EXPECT_EQ(3,        get<0>(get<0>(x)));
+    EXPECT_EQ(char(5),  get<1>(get<0>(x)));
+    EXPECT_EQ(true,     get<1>(x));
+    x = t ^ std::move(u);
+    EXPECT_EQ(2,        get<0>(get<0>(x)));
+    EXPECT_EQ(char(1),  get<1>(get<0>(x)));
+    EXPECT_EQ(false,    get<1>(x));
+    x = !x;
+    EXPECT_EQ(char(0),  get<0>(get<0>(x)));
+    EXPECT_EQ(char(0),  get<1>(get<0>(x)));
+    EXPECT_EQ(true,     get<1>(x));
+    x = make_tuple(~make_tuple(7,5), false);
+    EXPECT_EQ(char(250),get<1>(get<0>(x)));
+    EXPECT_EQ(false,    get<1>(x));
+}
+
+TEST(TupleTest, UnaryTuples) {
+    tuple<int> t{3};
+    tuple<tuple<int>> u{{2}};
+    tuple<tuple<double>> s{{3}};
+    u = tuple<tuple<long long>>{s};
+    EXPECT_EQ(3, get<0>(get<0>(u)));
+    u = t;
+    EXPECT_EQ(3, get<0>(get<0>(u)));
+}

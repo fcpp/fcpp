e5a34f983cc8e0c9109768904622a7e909df8651
Giorgio Audrito <giorgio.audrito@gmail.com>
2021-04-18 10:09:40 +0200

    removed unnamed namespace in header file

--- src/lib/common/tagged_tuple.hpp
+++ src/lib/common/tagged_tuple.hpp
@@ -179,36 +179,34 @@ namespace details {
         using type = typename tag_to_type<type_sequence<Ss...>, type_sequence<Ts...>, type_sequence<Us...>>::type::template push_front<typename type_sequence<Ts...,void>::template get<type_sequence<Ss...,void>::template find<U>>>;
     };
 
-    namespace {
-        //! @brief Separator between tuple tags and values.
-        template <typename T>
-        constexpr const char* tag_val_sep = "";
-        template<>
-        constexpr const char* tag_val_sep<tags::dictionary_tuple> = ":";
-        template<>
-        constexpr const char* tag_val_sep<tags::assignment_tuple> = " = ";
-        template<>
-        constexpr const char* tag_val_sep<tags::underscore_tuple> = "-";
-        template<>
-        constexpr const char* tag_val_sep<tags::arrowhead_tuple> = " => ";
+    //! @brief Separator between tuple tags and values.
+    template <typename T>
+    constexpr const char* tag_val_sep = "";
+    template<>
+    constexpr const char* tag_val_sep<tags::dictionary_tuple> = ":";
+    template<>
+    constexpr const char* tag_val_sep<tags::assignment_tuple> = " = ";
+    template<>
+    constexpr const char* tag_val_sep<tags::underscore_tuple> = "-";
+    template<>
+    constexpr const char* tag_val_sep<tags::arrowhead_tuple> = " => ";
 
-        //! @brief Separator between tuple values and the following tags.
-        template <typename T>
-        constexpr const char* val_tag_sep = "";
-        template<>
-        constexpr const char* val_tag_sep<tags::dictionary_tuple> = ", ";
-        template<>
-        constexpr const char* val_tag_sep<tags::assignment_tuple> = ", ";
-        template<>
-        constexpr const char* val_tag_sep<tags::underscore_tuple> = "_";
-        template<>
-        constexpr const char* val_tag_sep<tags::arrowhead_tuple> = "; ";
+    //! @brief Separator between tuple values and the following tags.
+    template <typename T>
+    constexpr const char* val_tag_sep = "";
+    template<>
+    constexpr const char* val_tag_sep<tags::dictionary_tuple> = ", ";
+    template<>
+    constexpr const char* val_tag_sep<tags::assignment_tuple> = ", ";
+    template<>
+    constexpr const char* val_tag_sep<tags::underscore_tuple> = "_";
+    template<>
+    constexpr const char* val_tag_sep<tags::arrowhead_tuple> = "; ";
 
-        std::string strip_namespaces_type(std::string s) {
-            size_t pos = s.rfind("::");
-            if (pos == std::string::npos) return s;
-            return s.substr(pos+2);
-        }
+    std::string strip_namespaces_type(std::string s) {
+        size_t pos = s.rfind("::");
+        if (pos == std::string::npos) return s;
+        return s.substr(pos+2);
     }
 
     //! @brief Removes the namespaces from a type representation.

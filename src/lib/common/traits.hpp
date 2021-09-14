015009d291df90f041f5b5b0ad0e04f1160b0ae9
Giorgio Audrito <giorgio.audrito@gmail.com>
2021-09-14 22:53:56 +0200

    empowering the number_sequence

--- src/lib/common/traits.hpp
+++ src/lib/common/traits.hpp
@@ -754,9 +754,166 @@ template <typename T>
 using type_unwrap = typename details::type_unwrap<T>::type;
 
 
-//! @brief Wraps a sequence of `intmax_t` values.
+/**
+ * @name number_sequence
+ *
+ * Helper empty class encapsulating a sequence of numbers.
+ * Mimics operations in standard stl containers.
+ */
+//! @{
+//! @brief General form.
 template <intmax_t... xs>
-struct number_sequence {};
+struct number_sequence;
+
+
+// Base case (0 if not found).
+template <intmax_t a, intmax_t... xs>
+constexpr size_t number_count = 0;
+
+// General recursive pattern.
+template <intmax_t a, intmax_t b, intmax_t... xs>
+constexpr size_t number_count<a, b, xs...> = number_count<a, xs...> + (a == b ? 1 : 0);
+
+// General recursive pattern.
+template <intmax_t a, intmax_t b, intmax_t... xs>
+constexpr size_t number_find = number_find<a, xs...> + 1;
+
+// Base case (the searched type is first).
+template <intmax_t a, intmax_t... xs>
+constexpr size_t number_find<a, a, xs...> = 0;
+
+
+//! @cond INTERNAL
+namespace details {
+    // General form.
+    template<int start, int end, int stride, intmax_t... xs>
+    struct number_slice {
+        using type = number_sequence<>;
+    };
+
+    // Recursive form.
+    template<int start, int end, int stride, intmax_t x, intmax_t... xs>
+    struct number_slice<start, end, stride, x, xs...> {
+        using type = std::conditional_t<end == 0, number_sequence<>, std::conditional_t<
+            start == 0,
+            typename number_slice<stride - 1, end - 1, stride, xs...>::type::template push_front<x>,
+            typename number_slice<start  - 1, end - 1, stride, xs...>::type
+        >>;
+    };
+}
+//! @endcond
+
+/**
+ * @brief Extracts a subsequence from the number sequence.
+ * @param start  first element extracted
+ * @param end    no element extracted after end (defaults to -1 = end of the sequence)
+ * @param stride interval between element extracted (defaults to 1)
+ */
+template <int start, int end, int stride, intmax_t... xs>
+using number_slice = typename details::number_slice<start, end, stride, xs...>::type;
+
+//! @brief Extracts the n-th type from the sequence.
+template <int n, intmax_t... xs>
+constexpr intmax_t number_get = number_slice<n, n+1, 1, xs...>::front;
+
+
+//! @brief Non-empty form, allows for extracting elements and subsequences.
+template <intmax_t x, intmax_t... xs>
+struct number_sequence<x, xs...> {
+    //! @brief Extracts the n-th number from the sequence.
+    template <int n>
+    static constexpr intmax_t get = number_get<n, x, xs...>;
+
+    /**
+     * @brief Extracts a subsequence from the number sequence.
+     * @param start  first element extracted
+     * @param end    no element extracted after end (defaults to -1 = end of the sequence)
+     * @param stride interval between element extracted (defaults to 1)
+     */
+    template <int start, int end = -1, int stride = 1>
+    using slice = number_slice<start, end, stride, x, xs...>;
+
+    //! @brief The first number of the sequence.
+    static constexpr intmax_t front = x;
+
+    //! @brief The last number of the sequence.
+    static constexpr intmax_t back = get<sizeof...(xs)>;
+
+    //! @brief Removes the first number of the sequence.
+    using pop_front = number_sequence<xs...>;
+
+    //! @brief Removes the last number of the sequence.
+    using pop_back = slice<0, sizeof...(xs)>;
+
+    //! @brief Adds types at the front of the sequence.
+    template <intmax_t... ys>
+    using push_front = number_sequence<ys..., x, xs...>;
+
+    //! @brief Adds types at the back of the sequence.
+    template <intmax_t... ys>
+    using push_back = number_sequence<x, xs..., ys...>;
+
+//    //! @brief Set intersection with other sequence.
+//    template<intmax_t... ys>
+//    using intersect = number_intersect<number_sequence<x, xs...>, number_sequence<ys...>>;
+//
+//    //! @brief Set union with other sequence.
+//    template<intmax_t... ys>
+//    using unite = number_unite<number_sequence<x, xs...>, number_sequence<ys...>>;
+//
+//    //! @brief Set difference with other sequence.
+//    template<intmax_t... ys>
+//    using subtract = number_subtract<number_sequence<x, xs...>, number_sequence<ys...>>;
+//
+//    //! @brief Extract the types that are repeated more than once.
+//    using repeated = number_repeated<x, xs...>;
+//
+//    //! @brief Extract the subsequence in which each type appears once (opposite of repeated).
+//    using uniq = number_uniq<x, xs...>;
+
+    //! @brief Constant equal to the index of `y` among the sequence. Fails to compile if not present, indices start from 0.
+    template <intmax_t y>
+    static constexpr size_t find = number_find<y, x, xs...>;
+
+    //! @brief Constant which is true if and only if the type parameter is in the sequence.
+    template <intmax_t y>
+    static constexpr size_t count = number_count<y, x, xs...>;
+
+    //! @brief The length of the sequence.
+    static constexpr size_t size = 1 + sizeof...(xs);
+};
+
+//! @brief Empty form, cannot extract elements and subsequences.
+template <>
+struct number_sequence<> {
+    template <int start, int end, int stride = 1>
+    using slice = number_sequence<>;
+
+    template <intmax_t... ys>
+    using push_front = number_sequence<ys...>;
+
+    template <intmax_t... ys>
+    using push_back = number_sequence<ys...>;
+
+    template<intmax_t... ys>
+    using intersect = number_sequence<>;
+
+    template<intmax_t... ys>
+    using unite = number_sequence<ys...>;
+
+    template<intmax_t... ys>
+    using subtract = number_sequence<>;
+
+    using repeated = number_sequence<>;
+
+    using uniq = number_sequence<>;
+
+    template <intmax_t y>
+    static constexpr size_t count = 0;
+
+    static constexpr size_t size = 0;
+};
+//! @}
 
 
 //! @cond INTERNAL
--- src/test/common/traits.cpp
+++ src/test/common/traits.cpp
@@ -101,6 +101,54 @@ TEST(TraitsTest, SearchOp) {
     EXPECT_EQ(2, t);
 }
 
+TEST(TraitsTest, NumberSequence) {
+    int i;
+    i = common::number_sequence<1,2,4,4,0>::front;
+    EXPECT_EQ(1, i);
+    i = common::number_sequence<0,2,4,4,1>::back;
+    EXPECT_EQ(1, i);
+    EXPECT_SAME(common::number_sequence<2,4,4,0>,
+                common::number_sequence<2,4,4,0>::push_back<>);
+    EXPECT_SAME(common::number_sequence<2,4,4,0>,
+                common::number_sequence<2,4,4>::push_back<0>);
+    EXPECT_SAME(common::number_sequence<2,4,4,0>,
+                common::number_sequence<2,4>::push_back<4,0>);
+    EXPECT_SAME(common::number_sequence<2,4,4,0>,
+                common::number_sequence<2>::push_back<4,4,0>);
+    EXPECT_SAME(common::number_sequence<2,4,4,0>,
+                common::number_sequence<>::push_back<2,4,4,0>);
+    EXPECT_SAME(common::number_sequence<2,4,4,0>,
+                common::number_sequence<4,0>::push_front<2,4>);
+    EXPECT_SAME(common::number_sequence<2,4,4,0>,
+                common::number_sequence<1,2,4,4,0>::pop_front);
+    EXPECT_SAME(common::number_sequence<2,4,4,0>,
+                common::number_sequence<2,4,4,0,1>::pop_back);
+    i = common::number_sequence<2,4,4,0>::size;
+    EXPECT_EQ(4, i);
+    i = common::number_sequence<>::size;
+    EXPECT_EQ(0, i);
+    EXPECT_SAME(common::number_sequence<4,0>,
+                common::number_sequence<2,4,4,0>::slice<1,-1,2>);
+    EXPECT_SAME(common::number_sequence<2,0>,
+                common::number_sequence<2,4,4,0>::slice<0,-1,3>);
+    EXPECT_SAME(common::number_sequence<4>,
+                common::number_sequence<2,4,4,0>::slice<2,-1,5>);
+    EXPECT_SAME(common::number_sequence<4,4>,
+                common::number_sequence<2,4,4,0>::slice<1,3,1>);
+    i = common::number_sequence<2,2,4,0>::get<2>;
+    EXPECT_EQ(4, i);
+    i = common::number_sequence<2,4,0>::find<2>;
+    EXPECT_EQ(0, i);
+    i = common::number_sequence<2,0,4>::find<4>;
+    EXPECT_EQ(2, i);
+    i = common::number_sequence<2,0,4>::count<4>;
+    EXPECT_EQ(1, i);
+    i = common::number_sequence<4,0>::count<2>;
+    EXPECT_EQ(0, i);
+    i = common::number_sequence<2,0,2>::count<2>;
+    EXPECT_EQ(2, i);
+}
+
 TEST(TraitsTest, ExportList) {
     EXPECT_SAME(common::export_list<>,
                 common::type_sequence<>);

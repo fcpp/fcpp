8b41195517d0c9d9c673fa9297b571e769d42d1f
Gianmarco Rampulla <45428938+denoide1@users.noreply.github.com>
2021-10-10 15:15:48 +0200

    - Optimized spacing and code format has been standardized
    - Optimized spacing and standardized code format
    -  Now load image assume images are on texture folder
    - Now if path is empty load image fills
    m_bitmap with a trivial 1x1 matrix containing false
    - is_obstacle_color has been removed and a more robust check has been introduced (now channels are considered correctly)
    - removed unnecessary parenthesis
    - introduced a runtime_error on image load failed
    - CMakeLists.txt has been optimized to avoid repeated code

--- src/lib/simulation/simulated_map.hpp
+++ src/lib/simulation/simulated_map.hpp
@@ -13,10 +13,8 @@
 #include "lib/data/color.hpp"
 #include "lib/common/traits.hpp"
 
-
 #include <cstring>
 #include "./external/stb_image/stb_image.h"
-
 
 /**
  * @brief Namespace containing all the objects in the FCPP library.
@@ -28,13 +26,12 @@ namespace component {
 
 //! @brief Namespace of tags to be used for initialising components.
 namespace tags {
-
     //! @brief Declaration tag associating to the dimensionality of the space.
-    template<intmax_t n>
+    template <intmax_t n>
     struct dimension;
 
     //! @brief Declaration tag associating to the bounding coordinates of the grid area.
-    template<intmax_t xmin, intmax_t ymin, intmax_t xmax, intmax_t ymax, intmax_t den>
+    template <intmax_t xmin, intmax_t ymin, intmax_t xmax, intmax_t ymax, intmax_t den>
     struct area;
 
     //! @brief Net initialisation tag associating to the minimum coordinates of the grid area.
@@ -54,9 +51,9 @@ namespace tags {
 
 }
 
+
 //! @cond INTERNAL
 namespace details {
-
     //! @brief Converts a number sequence to a vec (general form).
     template <typename T>
     struct numseq_to_vec_map;
@@ -72,12 +69,8 @@ namespace details {
         constexpr static vec<2> min{xmin*1.0/den,ymin*1.0/den};
         constexpr static vec<2> max{xmax*1.0/den,ymax*1.0/den};
     };
-    
 }
-
 //! @endcond
-
-
 
 
 /**
@@ -95,9 +88,8 @@ namespace details {
  * - \ref tags::obstacles_color_threshold associates to a real number used to have a margin error for colors in different image format.
  *
  */
-template<class... Ts>
+template <class... Ts>
 struct simulated_map {
-
     //! @brief The dimensionality of the space.
     constexpr static intmax_t dimension = common::option_num<tags::dimension, 2, Ts...>;
 
@@ -115,9 +107,8 @@ struct simulated_map {
      * @param F The final composition of all components.
      * @param P The parent component to inherit from.
      */
-    template<typename F, typename P>
+    template <typename F, typename P>
     struct component : public P {
-
         //! @cond INTERNAL
         DECLARE_COMPONENT(simulated_map);
         //! @endcond
@@ -129,13 +120,12 @@ struct simulated_map {
         class net : public P::net {
 
           public: // visible by node objects and the main program
-
             //! @brief Type for representing a position.
             using position_type = vec<dimension>;
 
             //! @brief Constructor from a tagged tuple.
-            template<typename S, typename T>
-            net(common::tagged_tuple <S, T> const &t) : P::net(t)  {
+            template <typename S, typename T>
+            net(const common::tagged_tuple <S, T>& t) : P::net(t) {
 
                 static_assert(area::size == 5 or S::template intersect<tags::area_min, tags::area_max>::size == 2,
                               "no option area defined and no area_min and area_max defined either");
@@ -143,55 +133,39 @@ struct simulated_map {
                 //variables to avoid linking issues
                 constexpr auto max = details::numseq_to_vec_map<area>::max;
                 constexpr auto min = details::numseq_to_vec_map<area>::min;
-
                 m_viewport_min = common::get_or<tags::area_min>(t, min);
                 m_viewport_max = common::get_or<tags::area_max>(t, max);
-
                 m_obstacles = common::get_or<tags::obstacles>(t, "");
-                m_color = common::get_or<tags::obstacles_color>(t, color(fcpp::BLACK));
+                m_color = common::get_or<tags::obstacles_color>(t, color(BLACK));
                 m_threshold = common::get_or<tags::obstacles_color_threshold>(t, 0.5);
 
                 load_bitmap(m_obstacles);
 
                 m_index_sizes = {m_bitmap[0].size(), m_bitmap.size()};
-
                 m_viewport_size = m_viewport_max - m_viewport_min;
 
             }
 
             //! @brief Returns the position of the closest empty space starting from node_position
             position_type closest_space(position_type position) {
-
                 if (!is_obstacle(position)) return position;
-
                 index_type index = position_to_index(position);
-
                 return index_to_position(m_closest[index[0]][index[1]], position);
-
             }
 
             //! @brief Returns the position of the closest obstacle starting from node_position
             position_type closest_obstacle(position_type position) {
-
                 if (is_obstacle(position)) return position;
-
                 if (!is_in_area(position)) return closest_obstacle(get_nearest_edge_position(position));
-
                 index_type index = position_to_index(position);
-
                 return index_to_position(m_closest[index[0]][index[1]], position);
-
             }
 
             //! @brief Returns true if a specific position is a obstacle otherwise false
             bool is_obstacle(position_type position) {
-
                 if (!is_in_area(position)) return false;
-
                 index_type index = position_to_index(position);
-
                 return m_bitmap[index[0]][index[1]];
-
             }
 
           private: // implementation details
@@ -200,95 +174,74 @@ struct simulated_map {
             using index_type = std::array<size_t, 2>;
 
             //! @brief Converts a node position to an equivalent bitmap index
-            inline index_type position_to_index(position_type const &position) {
-
+            inline index_type position_to_index(position_type const& position) {
                 index_type index_to_return;
-
                 //linear scaling
                 for(int i = 0; i < 2; i++)
-                    index_to_return[i] = static_cast<size_t>(std::round(((m_index_sizes[i] / m_viewport_size[i])) * (position[i] - m_viewport_min[i])));
-
+                    index_to_return[i] = static_cast<size_t>(std::round(m_index_sizes[i] / m_viewport_size[i] * (position[i] - m_viewport_min[i])));
                 return index_to_return;
-
             }
 
             //! @brief Converts a bitmap index to an equivalent node position
-            position_type index_to_position(index_type const &index, position_type position) {
-
+            position_type index_to_position(index_type const& index, position_type position) {
                 //linear scaling inverse formula
                 for(int i = 0; i < 2; i++)
-                    position[i] = (((index[i] / m_index_sizes[0])) * (m_viewport_size[i])) + m_viewport_min[i];
-
+                    position[i] = index[i] / m_index_sizes[i] * m_viewport_size[i] + m_viewport_min[i];
                 return position;
-
             }
 
             //! @brief Checks if a position is contained in the predefined area
             bool is_in_area(position_type position) {
-
                 for (int i = 0; i < 2; i++)
                     if (position[i] < m_viewport_min[i] || position[i] > m_viewport_max[i])
                         return false;
-
                 return true;
-
             }
 
             //! @brief Calculates the nearest in area position (edge position) starting from a generic node position
             position_type get_nearest_edge_position(position_type position) {
-
                 for (int i = 0; i < 2; i++)
                     position[i] = std::min(std::max(position[i], m_viewport_min[i]), m_viewport_max[i]);
-
                 return position;
-
             }
 
-
             //! @brief Fills m_bitmap by reading and parsing a stored bitmap given the bitmap file path
-            void load_bitmap(std::string const &path) {
+            void load_bitmap(std::string const& path) {
+                int bitmap_width, bitmap_height, channels_per_pixel, line_index = 0, j;
+                std::string real_path;
+                bool temp_var;
 
-                int bitmap_width, bitmap_height, channels_per_pixel, line_index = 0;
-                unsigned char *bitmap_data = stbi_load(path.c_str(), &bitmap_width, &bitmap_height, &channels_per_pixel, 0);
+                if(path.empty()) {
+                    m_bitmap = {{false}};
+                    return;
+                }
+
+                #if _WIN32
+                   real_path = std::string(".\\textures\\").append(path);
+                #else
+                   real_path = std::string("./textures/").append(path);
+                #endif
+
+                unsigned char *bitmap_data = stbi_load(real_path.c_str(), &bitmap_width, &bitmap_height, &channels_per_pixel, 0);
 
                 if (bitmap_data != nullptr) {
 
                     m_bitmap.emplace_back();
-
                     for (int i = 0; i * channels_per_pixel < bitmap_height * bitmap_width * channels_per_pixel; i++) {
-
                         unsigned char *pixelOffset = bitmap_data + i * channels_per_pixel;
-
                         if (i != 0 && i % bitmap_width == 0) {
                             line_index++;
                             m_bitmap.emplace_back();
                         }
-
-                        m_bitmap[line_index].push_back(is_obstacle_color(pixelOffset[0], pixelOffset[1], pixelOffset[2]));
+                        for(j = 0, temp_var = true; j < channels_per_pixel; j++)
+                            temp_var = temp_var && std::abs(m_color.rgba[j] - pixelOffset[j]) < m_threshold;
+                        m_bitmap[line_index].push_back(temp_var);
                     }
+                    delete bitmap_data;
 
-                } else {
-
-                    //how to handle error?
-
-                }
-
+                } else throw std::runtime_error("Error in image loading");
 
             }
-
-            /**
-            * @brief Returns if a color specified by rgb values is equal, considering threshold, to the defined obstacle color
-            *
-            * this method is used to fill m_bitmap with true or false values and is resposible to define wheter a pixel is an obstacle or not based on color
-            */
-            bool is_obstacle_color(int red, int green, int blue) {
-
-                return !(std::abs(m_color.red() - red) > m_threshold ||
-                         std::abs(m_color.green() - green) > m_threshold ||
-                         std::abs(m_color.blue() - blue) > m_threshold);
-
-            }
-
 
             /**
              * @brief Bitmap representation
@@ -333,7 +286,5 @@ struct simulated_map {
 }
 
 }
-
-
 
 #endif // FCPP_SIMULATED_MAP_H_

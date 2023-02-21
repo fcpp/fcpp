082c1b165d4fdd31b048e7dfdd20bd2fe55d2529
Giorgio Audrito <giorgio.audrito@gmail.com>
2023-02-21 14:02:18 +0100

    fixing printing issue with field<bool>

--- src/lib/common/ostream.hpp
+++ src/lib/common/ostream.hpp
@@ -64,6 +64,9 @@ namespace details {
 
 //! @brief String conversion of basic types.
 //! @{
+inline std::string to_string(bool x) {
+    return x ? "true" : "false";
+}
 inline std::string to_string(char x) {
     return {x};
 }
@@ -405,29 +408,31 @@ namespace fcpp {
     //! @brief Printing fields.
     template <typename O, typename T, typename = common::if_ostream<O>>
     O& operator<<(O& o, field<T> const& x) {
+        using const_ref = std::conditional_t<std::is_same<T,bool>::value, T, T const&>;
         o << "{";
         for (size_t i = 0; i < details::get_ids(x).size(); ++i) {
-            o << details::get_ids(x)[i] << ":" << common::escape(details::get_vals(x)[i+1]) << ", ";
+            o << details::get_ids(x)[i] << ":" << common::escape(const_ref(details::get_vals(x)[i+1])) << ", ";
         }
-        o << "*:" << common::escape(details::get_vals(x)[0]) << "}";
+        o << "*:" << common::escape(const_ref(details::get_vals(x)[0])) << "}";
         return o;
     }
 
     //! @brief Converting fields to strings.
     template <typename T, typename = fcpp::details::if_stringable<T>>
     std::string to_string(field<T> const& x) {
+        using const_ref = std::conditional_t<std::is_same<T,bool>::value, T, T const&>;
         std::string s = "{";
         for (size_t i = 0; i < details::get_ids(x).size() and i < FCPP_FIELD_DRAW_LIMIT and s.size() < 10*FCPP_FIELD_DRAW_LIMIT; ++i) {
             s += to_string(details::get_ids(x)[i]);
             s.push_back(':');
-            s += to_string(common::escape(details::get_vals(x)[i+1]));
+            s += to_string(common::escape(const_ref(details::get_vals(x)[i+1])));
             if (i+1 == details::get_ids(x).size() or (i < FCPP_FIELD_DRAW_LIMIT-1 and s.size() < 10*FCPP_FIELD_DRAW_LIMIT-2)) s.push_back(',');
             else for (int j=0; j<3; ++j) s.push_back('.');
             s.push_back(' ');
         }
         s.push_back('*');
         s.push_back(':');
-        s += to_string(common::escape(details::get_vals(x)[0]));
+        s += to_string(common::escape(const_ref(details::get_vals(x)[0])));
         s.push_back('}');
         return s;
     }

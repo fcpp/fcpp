88acbd4076081f788af6fc137517944dc794df76
danielminutolo <117604520+danielminutolo@users.noreply.github.com>
2023-06-09 10:48:30 +0200

    Aggiunta della funzione list_arith_collection e dei relativi test (#25)
    * Aggiornamenti
    
    * Modifica ultimo test
    
    * Pull request comments solved
    
    * Changing type for x
    
    * polishing files
    
    * removing spurious comment from randomizer
    
    * removing extra space
    
    ---------
    
    Co-authored-by: Daniel Minutolo <minutolo.daniel@educ.di.unito.it>
    Co-authored-by: Giorgio Audrito <giorgio.audrito@gmail.com>

--- src/test/coordination/collection.cpp
+++ src/test/coordination/collection.cpp
@@ -20,7 +20,8 @@ DECLARE_OPTIONS(options,
         coordination::sp_collection_t<int,real_t>,
         coordination::mp_collection_t<int,real_t>,
         coordination::wmp_collection_t<real_t>,
-        coordination::list_idem_collection_t<int>        
+        coordination::list_idem_collection_t<int>,
+        coordination::list_arith_collection_t<int>        
     >,
     export_pointer<(O & 1) == 1>,
     export_split<(O & 2) == 2>,
@@ -112,6 +113,63 @@ DECLARE_COMBINE(calc_dist1, lagdist1, component::calculus);
 
 template <int O>
 using combo1 = calc_dist1<options<O>>;
+
+template <class...>
+struct lagdist2 {
+    template <typename F, typename P>
+    struct component : public P {
+        struct node : public P::node {
+            using P::node::node;
+
+            field<real_t> nbr_dist() const {
+                device_t uid = P::node::uid;
+                if (uid == 0)
+                    return fcpp::details::make_field<real_t>({0,1,2}, {INF,0,5,5});
+                else if (uid == 1)
+                    return fcpp::details::make_field<real_t>({0,1,3,4}, {INF,5,0,1,3});
+                else if (uid == 2)
+                    return fcpp::details::make_field<real_t>({0,2,4,5}, {INF,5,0,5,3});             
+                else if (uid == 3)
+                    return fcpp::details::make_field<real_t>({1,3,6}, {INF,1,0,5});
+                else if (uid == 4)
+                    return fcpp::details::make_field<real_t>({1,2,4,6,7}, {INF,3,5,0,2,1});
+                else if (uid == 5)
+                    return fcpp::details::make_field<real_t>({2,5,7}, {INF,3,0,1});
+                else if (uid == 6)
+                    return fcpp::details::make_field<real_t>({3,4,6,8}, {INF,5,2,0,3});
+                else if (uid == 7)
+                    return fcpp::details::make_field<real_t>({4,5,7,8}, {INF,1,1,0,2});
+                else if (uid == 8)
+                    return fcpp::details::make_field<real_t>({6,7,8}, {INF,3,2,0});
+            }
+
+            field<real_t> nbr_lag() const {
+                return {1};
+            }
+
+            times_t current_time() const {
+                return m_t;
+            }
+
+            times_t next_time() const {
+                return m_t+1;
+            }
+
+            void round_start(times_t t) {
+                P::node::round_start(t);
+                m_t = t;
+            }
+
+          private:
+            times_t m_t;
+        };
+        using net = typename P::net;
+    };
+};
+DECLARE_COMBINE(calc_dist2, lagdist2, component::calculus);
+
+template <int O>
+using combo2 = calc_dist2<options<O>>;
 
 
 real_t adder(real_t x, real_t y) {
@@ -298,4 +356,134 @@ MULTI_TEST(CollectionTest, LISTidemGraph, O, 3) {
     EXPECT_ROUND(n, {0, 1, 2, 3},
                     {0, 1, 2, 10},
                     {10, 10, 2, 10});                            
+}
+
+MULTI_TEST(CollectionTest, LISTarith, O, 3) {
+    test_net<combo<O>, std::tuple<real_t>(int, int)> n{
+        [&](auto& node, int id, int val){
+            return std ::make_tuple(
+                coordination::list_arith_collection(node, 0, id, val, 2, 0, 0, 1,adder)
+            );
+        }
+    };
+
+    EXPECT_ROUND(n, {0, 1, 2},
+                {1, 2, 4},
+                {1, 2, 4});
+
+    EXPECT_ROUND(n, {0, 1, 2},
+                {1, 2, 4},
+                {3, 6, 4});
+
+    EXPECT_ROUND(n, {0, 1, 2},
+                {1, 2, 4},
+                {3, 6, 4});
+
+    EXPECT_ROUND(n, {0, 1, 2},
+                {1, 2, 4},
+                {7, 6, 4});
+
+    EXPECT_ROUND(n, {0, 1, 2},
+                {1, 2, 2},
+                {7, 6, 2});
+
+    EXPECT_ROUND(n, {0, 1, 2},
+                {1, 2, 2},
+                {7, 6, 2});
+
+    EXPECT_ROUND(n, {0, 1, 2},
+                {1, 2, 2},
+                {7, 4, 2});
+
+    EXPECT_ROUND(n, {0, 1, 2},
+                {1, 2, 2},
+                {7, 4, 2});
+
+    EXPECT_ROUND(n, {0, 1, 2},
+                {1, 2, 2},
+                {5, 4, 2});
+}
+
+MULTI_TEST(CollectionTest, LISTarithGraph, O, 3) {
+    using topo_type = std::vector<std::vector<int>>;
+    std::vector<real_t> distance {0,1,5,7}; 
+
+    test_net<combo1<0>, std::tuple<real_t>(int, int), 4> n{
+        {{0,1,2},{0,1,3},{0,2,3},{1,2,3}}, 
+        [&](auto& node, int id, int val){
+            return std::make_tuple(
+                coordination::list_arith_collection(node, 0, distance[id], val, 17, 0, 0, 1,adder)
+            );
+        }
+    };
+    EXPECT_ROUND(n, {0, 1, 2, 3},
+                    {0, 1, 2, 3},
+                    {0, 1, 2, 3});
+    EXPECT_ROUND(n, {0, 1, 2, 3},
+                    {0, 1, 2, 3},
+                    {2, 4, 5, 3});  
+    EXPECT_ROUND(n, {0, 1, 2, 3},
+                    {0, 1, 2, 3},
+                    {2, 4, 5, 3});   
+    EXPECT_ROUND(n, {0, 1, 2, 3},
+                    {0, 1, 2, 10},
+                    {5, 4, 5, 10});                                                             
+    EXPECT_ROUND(n, {0, 1, 2, 3},
+                    {2, 4, 2, 10},
+                    {7, 7, 5, 10});
+    EXPECT_ROUND(n, {0, 1, 2, 3},
+                    {0, 1, 2, 10},
+                    {5, 11, 12, 10});  
+    EXPECT_ROUND(n, {0, 1, 2, 3},
+                    {0, 1, 2, 10},
+                    {5, 11, 12, 10}); 
+    EXPECT_ROUND(n, {0, 1, 2, 3},
+                    {0, 1, 2, 10},
+                    {12, 11, 12, 10});                           
+}
+
+MULTI_TEST(CollectionTest, LISTarithSevenGraph, O, 3) {
+    using topo_type = std::vector<std::vector<int>>;
+    std::vector<real_t> distance {0,1,3,5,7,8,10,13,15}; 
+
+    test_net<combo2<0>, std::tuple<real_t>(int, int), 9> n{
+        {{0,1,2},{0,1,3,4},{0,2,4,5},{1,3,6},{1,2,4,6,7},{2,5,7},{3,4,6,8},{4,5,7,8},{6,7,8}}, 
+        [&](auto& node, int id, int val){
+            return std::make_tuple(
+                coordination::list_arith_collection(node, 0, distance[id], val, 11, 0, 0, 1,adder)
+            );
+        }
+    };
+
+    EXPECT_ROUND(n, {0, 1, 2, 3, 4, 5, 6, 7, 8},
+                    {0, 1, 2, 3, 4, 5, 6, 7, 8},
+                    {0, 1, 2, 3, 4, 5, 6, 7, 8});
+
+    EXPECT_ROUND(n, {0, 1, 2, 3, 4, 5, 6, 7, 8},
+                    {0, 1, 2, 3, 4, 5, 6, 7, 8},
+                    {2, 5, 7, 9, 11, 12, 14, 15, 8});
+
+    EXPECT_ROUND(n, {0, 1, 2, 3, 4, 5, 6, 7, 8},
+                    {0, 1, 2, 3, 4, 5, 6, 7, 8},
+                    {2, 5, 7, 9, 11, 12, 14, 15, 8});
+
+    EXPECT_ROUND(n, {0, 1, 2, 3, 4, 5, 6, 7, 8},
+                    {0, 1, 2, 3, 4, 5, 6, 7, 8},
+                    {7, 12, 14, 17, 19, 20, 14, 15, 8});
+
+    EXPECT_ROUND(n, {0, 1, 2, 3, 4, 5, 6, 7, 8},
+                    {0, 1, 2, 3, 4, 5, 6, 7, 8},
+                    {7, 12, 14, 17, 19, 20, 14, 15, 8});
+
+    EXPECT_ROUND(n, {0, 1, 2, 3, 4, 5, 6, 7, 8},
+                    {0, 1, 2, 3, 4, 5, 6, 7, 8},
+                    {14, 20, 22, 17, 19, 20, 14, 15, 8});
+
+    EXPECT_ROUND(n, {0, 1, 2, 3, 4, 5, 6, 7, 8},
+                    {0, 1, 2, 3, 4, 5, 6, 7, 8},
+                    {14, 20, 22, 17, 19, 20, 14, 15, 8});
+
+    EXPECT_ROUND(n, {0, 1, 2, 3, 4, 5, 6, 7, 8},
+                    {0, 1, 2, 3, 4, 5, 6, 7, 8},
+                    {22, 20, 22, 17, 19, 20, 14, 15, 8});
 }
